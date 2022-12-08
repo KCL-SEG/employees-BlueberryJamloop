f99eefb9fe05311963440f5e451afc190df973b3
Maja G <maja293@outlook.com>
2022-12-08 15:37:36 +0000

    done

--- employee.py
+++ employee.py
@@ -1,52 +1,70 @@
 """Employee pay calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 class Employee:
-    def __init__(self, name, salary, hours_worked = 0, commission = "", commission_amount = 0, contracts_done = 0):
+    def __init__(self, name, commission):
         self.name = name
         self.commission = commission
-        self.hours_worked = hours_worked
-        self.salary = salary
-        self.commission_amount = commission_amount
-        self.contracts_done = contracts_done
 
     def get_pay(self):
-        if self.hours_worked != 0:
-            self.hourly_pay = self.hours_worked * self.salary
-            if self.commission != False:
-                if self.commision == "Bonus":
-                    self.hourly_pay += self.commission_amount
-                else:
-                    self.hourly_pay += self.commission_amount * self.contracts_done
-            return self.hourly_pay
-        else:
-            self.monthly_pay = self.salary
-            if self.commission != False:
-                if self.commision == "Bonus":
-                    self.monthly_pay += self.commission_amount
-                else:
-                    self.monthly_pay += self.commission_amount * self.contracts_done
-            return self.monthly_pay
-            
+        return self.get_base_pay() + (self.commission.value if self.commission else 0)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} works on a {self.get_contract_desc()}" + \
+            (f" and receives a {self.commission.description}" if self.commission else "") + \
+                f". Their total pay is {self.get_pay()}."
+
+class SalaryEmployee(Employee):
+    def __init__(self, name, salary, commission=None):
+        super().__init__(name, commission)
+        self.salary = salary
+
+    def get_contract_desc(self):
+        return f"monthly salary of {self.salary}"
+    
+    def get_base_pay(self):
+        return self.salary
+
+class HourlyEmployee(Employee):
+    def __init__(self, name, hours, pay_per_hour, commission=None):
+        super().__init__(name, commission)
+        self.hours = hours
+        self.pay_per_hour = pay_per_hour
+
+    def get_contract_desc(self):
+        return f"contract of {self.hours} hours at {self.pay_per_hour}/hour"
+
+    def get_base_pay(self):
+        return self.hours * self.pay_per_hour
+
+
+class FixedComm:
+    def __init__(self, bonus) -> None:
+        self.description = f"bonus commission of {bonus}"
+        self.value = bonus
+
+class ContractComm:
+    def __init__(self, contract_num, price_per_contract) -> None:
+        self.description = f"commission for {contract_num} contract(s) at {price_per_contract}/contract"
+        self.value = contract_num * price_per_contract
+
+
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie', 4000)
+billie = SalaryEmployee("Billie", 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', 25, 100)
+charlie = HourlyEmployee("Charlie", 100, 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee', 3000, 0, "Contract", 200, 4)
+renee = SalaryEmployee("Renee", 3000, ContractComm(4, 200))
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan', 25, 150, "Contract", 220, 3)
+jan = HourlyEmployee("Jan", 150, 25, ContractComm(3, 220))
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie', 2000, 0, "Bonus", 1500, 0)
+robbie = SalaryEmployee("Robbie", 2000, FixedComm(1500))
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel', 30, 120, "Bonus", 600, 0)
+ariel = HourlyEmployee("Ariel", 120, 30, FixedComm(600))
